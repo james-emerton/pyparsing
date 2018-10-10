43c519ed93cb151f76e31da178e9b2aa4dab8be3
James Emerton <james@emerton.info>
2018-10-10 15:51:00 -0700

    Prevent reference cycle on py3
    Python 3 exception objects have a `__traceback__` attribute that refers back to the current frame, thus causing a reference cycle.

--- pyparsing.py
+++ pyparsing.py
@@ -3603,26 +3603,30 @@ class MatchFirst(ParseExpression):
     def parseImpl( self, instring, loc, doActions=True ):
         maxExcLoc = -1
         maxException = None
-        for e in self.exprs:
-            try:
-                ret = e._parse( instring, loc, doActions )
-                return ret
-            except ParseException as err:
-                if err.loc > maxExcLoc:
-                    maxException = err
-                    maxExcLoc = err.loc
-            except IndexError:
-                if len(instring) > maxExcLoc:
-                    maxException = ParseException(instring,len(instring),e.errmsg,self)
-                    maxExcLoc = len(instring)
+        try:
+            for e in self.exprs:
+                try:
+                    ret = e._parse( instring, loc, doActions )
+                    return ret
+                except ParseException as err:
+                    if err.loc > maxExcLoc:
+                        maxException = err
+                        maxExcLoc = err.loc
+                except IndexError:
+                    if len(instring) > maxExcLoc:
+                        maxException = ParseException(instring,len(instring),e.errmsg,self)
+                        maxExcLoc = len(instring)
 
-        # only got here if no expression matched, raise exception for match that made it the furthest
-        else:
-            if maxException is not None:
-                maxException.msg = self.errmsg
-                raise maxException
+            # only got here if no expression matched, raise exception for match that made it the furthest
             else:
-                raise ParseException(instring, loc, "no defined alternatives to match", self)
+                if maxException is not None:
+                    maxException.msg = self.errmsg
+                    raise maxException
+                else:
+                    raise ParseException(instring, loc, "no defined alternatives to match", self)
+        finally:
+            # Break reference cycle
+            maxException = None
 
     def __ior__(self, other ):
         if isinstance( other, basestring ):
